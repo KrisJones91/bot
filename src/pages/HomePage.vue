31e2783b3cc2453a578f7fc2384d78703e2abf73
Kris Jones <kristop.jones@gmail.com>
2021-04-20 14:48:57 -0600

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home container-fluid">
+    <div class="row justify-content-center">
+      <h2>Want to achieve the exposure you deserve?</h2>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-4 text-center">
+        <p><small>You've come to the right place! Social Buzz allows you to <b> Purchase</b> followers, likes, and views for your favorite social media outlet. </small></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
