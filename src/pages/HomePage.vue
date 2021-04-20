5049b5c08ca93a52ced9ffdda29b81c3aa4e94a2
Kris Jones <kristop.jones@gmail.com>
2021-04-20 15:26:13 -0600

    deleted footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home bg-dark container-fluid">
+  <div class="home bg-main container-fluid">
     <div class="row justify-content-center">
       <div class="col-6 text-center">
         <h1>Increase your social media exposure</h1>
@@ -10,23 +10,26 @@
         <p class="m-0">
           Have you always wanted to become an influencer?
         </p>
-        <p class="m-0">
+        <!-- <p class="m-0">
           Want to grow your follower count?
-        </p>
+        </p> -->
         <p>Let <b>Social Buzz</b> help you get the <em class="text-danger">buzz</em> you've always deserved!</p>
       </div>
     </div>
     <div class="row justify-content-center">
       <div class="col-10">
+        <h1 class="text-center">
+          How it works:
+        </h1>
         <div class="card">
           <div class="row">
-            <div class="col">
-              <h1>step 1</h1>
+            <div class="col text-center">
+              <h1>Step 1</h1>
             </div>
-            <div class="col">
+            <div class="col text-center">
               <h1>Step 2</h1>
             </div>
-            <div class="col">
+            <div class="col text-center">
               <h1>Step 3</h1>
             </div>
           </div>
