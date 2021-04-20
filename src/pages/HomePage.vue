cd3cf525f64f6ba0ea12d94c1fc1b9f942b61449
Kris Jones <kristop.jones@gmail.com>
2021-04-20 15:21:07 -0600

    homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="home container-fluid">
+  <div class="home bg-dark container-fluid">
     <div class="row justify-content-center">
-      <h2>Want to achieve the exposure you deserve?</h2>
+      <div class="col-6 text-center">
+        <h1>Increase your social media exposure</h1>
+      </div>
     </div>
     <div class="row justify-content-center">
-      <div class="col-4 text-center">
-        <p><small>You've come to the right place! Social Buzz allows you to <b> Purchase</b> followers, likes, and views for your favorite social media outlet. </small></p>
+      <div class="col-6 text-center">
+        <p class="m-0">
+          Have you always wanted to become an influencer?
+        </p>
+        <p class="m-0">
+          Want to grow your follower count?
+        </p>
+        <p>Let <b>Social Buzz</b> help you get the <em class="text-danger">buzz</em> you've always deserved!</p>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-10">
+        <div class="card">
+          <div class="row">
+            <div class="col">
+              <h1>step 1</h1>
+            </div>
+            <div class="col">
+              <h1>Step 2</h1>
+            </div>
+            <div class="col">
+              <h1>Step 3</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
