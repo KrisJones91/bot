6564ef637017b8dd3873e2bbf66bf1e615fde2c2
Kris Jones <kristop.jones@gmail.com>
2021-05-02 15:32:36 -0600

    follower purchase

--- src/pages/InstagramPage.vue
+++ src/pages/InstagramPage.vue
@@ -26,20 +26,133 @@
     </div>
     <div class="row mt-3 justify-content-center">
       <div class="col-3">
-        <div class="card  text-center">
+        <div class="card card-two text-white text-center">
           <h4 class="mt-2 mb-0">
             BUY
           </h4>
           <h5 class="mb-3">
-            <b>200 followers</b>
+            <b>250 followers</b>
           </h5>
-          <h2><b>$4.99</b></h2>
-          <button class="btn btn-dark m-1">
+          <h2 class="pb-2">
+            <b>$2.99</b>
+          </h2>
+          <button class="btn btn-dark">
             Purchase
           </button>
-          <p class="m-1">
-            <em><small>Recieve in 7 business days</small></em>
-          </p>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>500 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$4.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>1000 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$11.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>1500 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$17.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3 justify-content-center">
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>2000 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$23.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>5000 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$52.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>7000 followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$72.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card card-two text-white text-center">
+          <h4 class="mt-2 mb-0">
+            BUY
+          </h4>
+          <h5 class="mb-3">
+            <b>10k followers</b>
+          </h5>
+          <h2 class="pb-2">
+            <b>$102.99</b>
+          </h2>
+          <button class="btn btn-dark">
+            Purchase
+          </button>
         </div>
       </div>
     </div>
@@ -68,7 +181,10 @@ export default {
   border: transparent;
   border-radius: 15px;
 }
-.card{
-  border-radius: 15px;
+.card-two{
+  border-radius: 10px;
+  background-image:linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13063, #fd1d1d);
+  border: none;
 }
+
 </style>
